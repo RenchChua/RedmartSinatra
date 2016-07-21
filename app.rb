ecbbf0b4d5d61b3226b18245533da43c8d50fa49
Rencheng Chua <rench00@gmail.com>
2016-07-21 09:51:25 +0800

    do about page

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ class RedmartSinatra < Sinatra::Base
     erb 'Hello World!'
   end
 
+  get '/about' do
+    erb 'This is where we tell you about us'
+  end
+
   get '/users' do
     @users = User.all
     erb :'users'
