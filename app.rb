2c3e85ef270b6e091e3585819e826e3556200edd
Rencheng Chua <rench00@gmail.com>
2016-07-22 10:15:40 +0800

    get closer to mvc model by creating more folders

--- app.rb
+++ app.rb
@@ -7,9 +7,33 @@ class RedmartSinatra < Sinatra::Base
     erb 'This is where we tell you about us'
   end
 
-  get '/users' do
+
+
+  get '/index' do
     @users = User.all
-    erb :'users'
+    erb :'./users/index'
+  end
+
+  #get form view to create new user
+  get "/users/new" do
+    @pizza = User.new
+    erb :"./users/new"
+  end
+
+  # creating a pizza, where the new form POSTs to, it does the actual creation
+
+  post "/users" do
+    @user = User.new(params[:user])
+    if @user.save
+      redirect("/users/#{@user.id}")
+    else
+      erb(:"user/new")
+    end
+  end
+
+  # get form view to edit an existing user
+  get "/users/:id/edit" do
+    erb "get form to edit one user with id #{params[:id]}"
   end
 
   get '/users/:id' do
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,12 @@ require './models/user'
 require 'Faker'
 
 (1..10).each do |i|
-  User.create(
+  @user = User.create(
   name: Faker::Name.name,
   email: Faker::Internet.email,
   address: Faker::Address.street_address,
   password: Faker::Internet.password(8),
   cc_number: Faker::Business.credit_card_number)
+
+  # @user.update_attributes(:email, Faker::Internet.email("#{@user[:name]}"))
 end
